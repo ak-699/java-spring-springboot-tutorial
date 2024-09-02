e79029e7a67e5690ef085a1ddcb8bb465c4b0f03
Abhishek Kumar <kumar699.abhishek@gmail.com>
2024-09-03 00:49:31 +0530

    Update loose coupling to integrate Web Service object

--- src/main/java/com/loose/coupling/LooseCouplingExample.java
+++ src/main/java/com/loose/coupling/LooseCouplingExample.java
@@ -2,7 +2,8 @@ package com.loose.coupling;
 
 public class LooseCouplingExample {
     public static void main(String[] args) {
-        UserDataProvider userDataProvider = new UserDatabaseProvider();
+        // UserDataProvider userDataProvider = new UserDatabaseProvider();
+        UserDataProvider userDataProvider = new WebServiceDatabaseProvider();
         UserManager userManager = new UserManager(userDataProvider);
         System.out.println(userManager.getUserInfo());
     }
