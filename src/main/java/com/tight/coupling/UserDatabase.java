ab099e2a2f24a65fcf112f7eee307ea9dcf121e1
Abhishek Kumar <abhishek.kumar@stayflexi.com>
2024-09-02 23:53:56 +0530

    Update UserDatabase class method

--- src/main/java/com/tight/coupling/UserDatabase.java
+++ src/main/java/com/tight/coupling/UserDatabase.java
@@ -2,6 +2,7 @@ package com.tight.coupling;
 
 public class UserDatabase {
     public String getUserDetails() {
+        // Directly access database and return some information
         return "User Details from database";
     }
 }
