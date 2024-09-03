70754a6802daa75cd05bdc2e15aa374a8530e2f9
Abhishek Kumar <kumar699.abhishek@gmail.com>
2024-09-03 12:08:13 +0530

    Add properties and methods for Car

--- src/main/java/car/example/constructor/injection/Car.java
+++ src/main/java/car/example/constructor/injection/Car.java
@@ -1,4 +1,8 @@
 package car.example.constructor.injection;
 
 public class Car {
+    private Specification carSpecification;
+    public void displayDetails() {
+        System.out.println("Car details: "+carSpecification);
+    }
 }
