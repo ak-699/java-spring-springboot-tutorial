d092936456134ae621ac7176d6285ea5ac5de728
Abhishek Kumar <kumar699.abhishek@gmail.com>
2024-09-04 14:29:28 +0530

    Add flow for autowire by name

--- src/main/java/com/example/autowire/name/App.java
+++ src/main/java/com/example/autowire/name/App.java
@@ -10,3 +10,18 @@ public class App {
         myCar.displayDetails();
     }
 }
+// first configuration is loaded and lookedup
+// spring sees there is a bean that needs to be created
+// it sees it needs to set the property and sets the property using setter
+// the first bean is done
+
+// then it goes to second bean
+// spring now needs to manage the second bean
+// and it needs to be autowire it by name
+// the name of the bean that needs to be wired to myCar object should be same as property name within car class
+// so myCar class contains a property called specification
+// spring will search for a bean with name same as property name
+// do we have a bean that matches this name? yes
+// then that bean is injected
+// bean is injected to myClass using corresponding setter as it is autowired by name
+// if no setter then there will be error
