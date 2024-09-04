d72c13e3da549948b359ab9676ad6588235240f3
Abhishek Kumar <kumar699.abhishek@gmail.com>
2024-09-04 11:09:24 +0530

    Implement setter injection

--- src/main/java/car/example/setter/injection/App.java
+++ src/main/java/car/example/setter/injection/App.java
@@ -1,11 +1,11 @@
-package car.example.constructor.injection;
+package car.example.setter.injection;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationConstructorInjection.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationSetterInjection.xml");
         Car myCar = (Car) context.getBean("myCar");
         myCar.displayDetails();
     }
