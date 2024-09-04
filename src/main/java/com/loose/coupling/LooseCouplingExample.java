ead9697af6212fa8b26e063af81134d7478fbd79
Abhishek Kumar <kumar699.abhishek@gmail.com>
2024-09-04 12:34:10 +0530

    Convert all database providers to bean

--- src/main/java/com/loose/coupling/LooseCouplingExample.java
+++ src/main/java/com/loose/coupling/LooseCouplingExample.java
@@ -1,16 +1,21 @@
 package com.loose.coupling;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class LooseCouplingExample {
     public static void main(String[] args) {
-        UserDataProvider userDatabaseProvider = new UserDatabaseProvider();
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationLooseCoupling.xml");
+
+        UserDataProvider userDatabaseProvider = (UserDataProvider) context.getBean("myDatabaseProvider");
         UserManager userManager = new UserManager(userDatabaseProvider);
         System.out.println(userManager.getUserInfo());
 
-        UserDataProvider userWebServiceDatabaseProvider = new WebServiceDatabaseProvider();
+        UserDataProvider userWebServiceDatabaseProvider = (UserDataProvider) context.getBean("myWebServiceDatabaseProvider");
         userManager = new UserManager(userWebServiceDatabaseProvider);
         System.out.println(userManager.getUserInfo());
 
-        UserDataProvider userNewDatabaseProvider = new NewDatabaseProvider();
+        UserDataProvider userNewDatabaseProvider = (UserDataProvider) context.getBean("myNewDatabaseProvider");
         userManager = new UserManager(userNewDatabaseProvider);
         System.out.println(userManager.getUserInfo());
     }
